24473d79a0a9458313afeb56179e926913ecf07c
李伟民 <1060395480@qq.com>
2023-08-07 18:57:20 +0800

    冲突测试_3

--- txbank/src/test/java/CT_Test.java
+++ txbank/src/test/java/CT_Test.java
@@ -6,6 +6,7 @@
  */
 public class CT_Test {
     public static void main(String[] args) {
-     System.out.println("456");
+     System.out.println("456986754678");
+     System.out.println("9876e5w456789098765");
     }
 }
